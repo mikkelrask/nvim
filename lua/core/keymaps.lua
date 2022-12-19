5849333267bfbf029a6b5d51358e23f55dad020e
mikkelrask@pm.me <mikkelrask@pm.me>
2022-12-19 18:09:31 +0100

    init (lol)

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.autowrite = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.smarttab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.laststatus = 2
+vim.opt.cursorline = true
+vim.opt.autoread = true
+--vim.opt.syntax = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
---
+++ lua/core/plugin_config/nvim-tree.lua
@@ -0,0 +1,21 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup({
+  auto_reload_on_write = true,
+  sort_by = "name",
+  view = {
+    adaptive_size = true,
+    mappings = {
+      list = {
+        { key = 'u', action = 'dir_up' },
+        { key = 'l', action = 'edit', action_cb = edit_or_open },
+        { key = 's', action = 'vsplit_preview', action_cb = vsplit_preview },
+        { key = 'h', action = 'close_node'},
+        { key = 'H', action = 'collapse_all', action_cb = collapse_all },
+      },
+    },
+  },
+})
+
+vim.keymap.set('n', '<c-b>', ':NvimTreeFindFileToggle<CR>')
