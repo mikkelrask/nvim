ed643e34d3c99bad31a9c54a603d3ed9029bb704
Mikkel Rask <mikkelrask@pm.me>
2024-02-11 10:26:46 +0100

    Telescope
    Bump Telescope from 1.1.0 to 1.1.5\nAdded plenary, ripgrep and fd as deps for search

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,7 +1,9 @@
-local builtin = require('telescope.builtin')
-
-vim.keymap.set('n', '<c-p>', builtin.find_files, {})
-vim.keymap.set('n', '<Space><Space>', builtin.git_files, {})
-vim.keymap.set('n', '<Space>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<Space>fh', builtin.help_tags, {})
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+      dependencies = { 
+	'nvim-lua/plenary.nvim',
+	'BurntSushi/ripgrep',
+   	'sharkdp/fd'
+}
+}
 
