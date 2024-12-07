dc878bcc451be6c2a0302cb884b6c42e2a9e3e40
Mikkel Rask <mikkelrask@pm.me>
2024-12-07 19:37:25 +0100

    Highlight on YOINK

---
+++ lua/custom/hightlightonyank.lua
@@ -0,0 +1,9 @@
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = "Highlight on yank",
+  group = vim.api.nvim_create_augroup('kickstart-hightlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+    print('yoink')
+  end
+})
--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,1 +1,2 @@
 require('custom.undercurl')
+require('custom.hightlightonyank')
