285c88770062a431647f282b0c7fb1d55bcd6fe6
Mikkel Rask <mikkelrask@pm.me>
2024-12-15 15:20:23 +0100

    copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,24 +1,24 @@
 return {
-	'zbirenbaum/copilot.lua',
-	cmd = "Copilot",
-	event = "InsertEnter",
-	config = function()
-		require('copilot').setup({
-			suggestion = {
-				enabled = false
-			},
-			panel = {
-				enabled = false
-			},
-			config = {
-				keymap = {
-					next = "<C-j>",
-					prev = "<C-k>",
-					select = "<CR>",
-					close = "<C-e>",
-					quit = "<C-q>"
-				}
-			}
-		})
-	end
+  'zbirenbaum/copilot.lua',
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require('copilot').setup({
+      suggestion = {
+        enabled = true
+      },
+      panel = {
+        enabled = false
+      },
+      config = {
+        keymap = {
+          next = "<C-j>",
+          prev = "<C-k>",
+          select = "<CR>",
+          close = "<C-e>",
+          quit = "<C-q>"
+        }
+      }
+    })
+  end
 }
