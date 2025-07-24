b5c359ce053956ae07d7b6e3d02ef53a7b6d8753
Mikkel Rask <mikkelrask@protonmail.com>
2025-07-24 15:14:56 +0200

    fix: 🩹 Fix having stowed dotfiles pwd set to ~/dotfiles Opening any symlinked files inside .config will no longer result in Oil or nvim tree working from the root of ~/dotfiles (which holds the     nearest `.git` directory) by making an ignore rule based on the subdirectory names in said dotfiles directory. Basically comparing "if this subdirectory of .config also exists in ~/dotfiles dont change the work directory" and instead sets it as the directory of whatever file we opened.
    I couldn't find any ways to get neither `nvim-rooter` or `project.nvim`
    to simply ignore symlinks, so this was the best of the hacky solutions I
    could figure out. And it WASN'T thanks to any LLM out there, their
    solutions sucked ass and tits.
    
    **Plugin changes:**
    + project.nvim
    - nvim-rooter
    
    Changes to be committed:
    	modified:   lua/plugins/rooter.lua

--- lua/plugins/rooter.lua
+++ lua/plugins/rooter.lua
@@ -1,6 +1,55 @@
 return {
-  'notjedi/nvim-rooter.lua',
+  'ahmedkhalf/project.nvim',
   config = function()
-    require('nvim-rooter').setup()
-  end
+    -- Project.nvim setup
+    require('project_nvim').setup({
+      detection_methods = { 'pattern' },
+      patterns = { '.git' },
+      silent_chdir = true,
+      scope_chdir = 'local', -- prøv local scope for mindre global cwd-skift
+    })
+
+    -- Her henter vi undermapper i ~/dotfiles
+    local uv = vim.loop
+    local dotfiles_path = vim.fn.expand("~/dotfiles")
+    local handle = uv.fs_scandir(dotfiles_path)
+
+    local folders = {}
+    if handle then
+      while true do
+        local name, type = uv.fs_scandir_next(handle)
+        if not name then break end
+        if type == 'directory' then
+          table.insert(folders, name)
+        end
+      end
+    end
+
+    -- Autocmd der sikrer cwd sættes til filens mappe
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function()
+        local filepath = vim.fn.expand('%:p')
+        if filepath == '' then return end
+
+        local file_dir = vim.fn.expand('%:p:h')
+        local cwd = vim.fn.getcwd()
+        local home = vim.fn.expand('~')
+
+        local in_dotfiles = false
+        for _, folder in ipairs(folders) do
+          local prefix = home .. '/.config/' .. folder .. '/'
+          if filepath:sub(1, #prefix) == prefix then
+            in_dotfiles = true
+            break
+          end
+        end
+
+        -- Skift cwd hvis filen er i dotfiles config eller cwd != filens mappe
+        if in_dotfiles or file_dir ~= cwd then
+          vim.cmd('silent! cd ' .. vim.fn.fnameescape(file_dir))
+          -- vim.notify('Changed cwd to ' .. file_dir .. ' for ' .. filepath) -- evt. debug
+        end
+      end,
+    })
+  end,
 }
