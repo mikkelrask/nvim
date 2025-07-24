1f566dd3ea85cbd3630580cdaa9bcb56aedfb609
Mikkel Rask <mikkelrask@protonmail.com>
2025-07-24 15:22:36 +0200

    refactor: 🔨 Change completion accept from ^Y to Tab

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -9,7 +9,7 @@ return {
       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
       -- see the "default configuration" section below for full documentation on how to define
       -- your own keymap.
-      keymap = { preset = 'default' },
+      keymap = { preset = 'super-tab' },
 
       appearance = {
         -- Sets the fallback highlight groups to nvim-cmp's highlight groups
@@ -34,7 +34,6 @@ return {
 
       completion = {
         accept = {
-          -- experimental auto-brackets support
           auto_brackets = {
             enabled = true,
           },
@@ -46,7 +45,7 @@ return {
         },
         documentation = {
           auto_show = true,
-          auto_show_delay_ms = 200,
+          auto_show_delay_ms = 100,
         },
       },
     })
